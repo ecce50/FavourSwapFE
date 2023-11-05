29d621dd62f5a2427de92265698a96e930891fbf
Peter R <prodger2@gmail.com>
2023-11-05 10:53:42 +0100

    token auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-
+import Profile from "./pages/Profile";
 
 
 function App() {
@@ -12,10 +12,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/login" element={<Login/>} />
-
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </>
   );
---
+++ src/context/Auth.context.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+import { createContext, useState } from "react";
+import axios from "axios";
+
+const AuthContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+const AuthContextWrapper = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const authenticateUser = async () => {
+    const tokenInStorage = localStorage.getItem("authToken");
+    console.log("Here is the token from the local storage", tokenInStorage);
+    if (tokenInStorage) {
+      // we make a call to the server to check if the token is valid. Delete line below when we call 'data'
+
+      const { data } = await axios.get("http://localhost:5005/auth/verify", {
+        headers: { authorization: `Bearer ${tokenInStorage}` },
+      });
+        console.log("From the context, here is the verify response ", data);
+        setUser(data.currentUser);
+    } else {
+      //we will set the user back to null, set isLoading to false, isLogged in to false
+    }
+  };
+  return (
+    <AuthContext.Provider value={{ authenticateUser, user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthContextWrapper };
