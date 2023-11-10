66c4949cece862d0a3136fc5b880f1a4e310549c
Mariana Ribeiro de Carvalho <mariana.rcarvalho.992gmail.com>
2023-11-10 10:43:58 +0000

    private route fixed, automatic login, logout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
 import { AuthContext } from "../context/Auth.context"
 import { useContext } from "react"
+import Logout from "../components/Logout"
 
 function Profile() {
     const { authenticateUser, user } = useContext(AuthContext)
-    //authenticateUser()
 
  
  
@@ -12,6 +12,7 @@ function Profile() {
         <>
         <div>Profile</div>
             <h3>Welcome {user ? user.email : null} </h3>
+        <Logout/>
       </>
   )
 }
