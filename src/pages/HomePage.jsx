81b5aadecbafd1e03f2a6cbfc908a93a19753b54
Mariana Ribeiro de Carvalho <mariana.rcarvalho.992gmail.com>
2023-11-20 10:53:01 +0000

    HomePage features except search-related

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,21 @@
-import { Link } from "react-router-dom";
+import SearchBar from "../components/SearchBar";
+import '../App.css';
+
 
 function HomePage() {
   return (
     <div>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/login">Log in</Link>
-      <Link to="/profile">Profile</Link>
+
+      <h1>Skill Ƨwap</h1>
+
+      <h4>Find Your Next Skill</h4>
+      <SearchBar/>
+
+      <h4>Want to Teach Others?</h4>
+      <a href="/profile">Get Started!</a>
+
+
+
     </div>
   );
 }
